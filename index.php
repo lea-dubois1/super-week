931e612a27ec010143275ac11e02156c19d64fa9
lea-dubois1 <lea.dubois@laplateforme.io>
2023-05-02 16:15:16 +0200

    ajout .gitignore

--- index.php
+++ index.php
@@ -19,6 +19,39 @@ $router->map('GET', '/users/[i:id]?', function($id) {
     echo "<h1>Bienvenu sur la page de l'utilisateur " . $id . "</h1>";
 }, 'user');
 
+$router->map('GET', '/addUser', function() {
+
+    require_once 'vendor/autoload.php';
+
+    $faker = Faker\Factory::create("fr_FR");
+    
+    $conn = new \PDO('mysql:host=localhost;dbname=revisions', "root", "");
+
+    $sql = "INSERT INTO user (email, first_name, last_name) VALUES (:email, :first_name, :last_name)";
+    $req = $conn->prepare($sql);
+
+    for ($i=0; $i < 10; $i++) {
+
+        $req->execute([':email' => $faker->email(),
+                       ':first_name' => $faker->firstName(),
+                       ':last_name' => $faker->lastName()
+        ]);
+
+    }
+
+    // for ($j=0; $j < 10; $j++) {
+
+    //     $titre = $faker->word();
+    //     $content = $faker->realTextBetween($minNbChars = 200, $maxNbChars = 260, $indexSize = 2);
+    //     $id_user = $faker->randomDigitNotNull();
+
+    //     $sqlBook = "INSERT INTO book (titre, content, id_user) VALUES (" . $titre . ", " . $content . ", " . $id_user . ")";
+    //     $reqBook = $conn->prepare($sqlBook);
+    //     $reqBook->execute();
+
+    // }
+}, "add");
+
 
 
 $match = $router->match();
