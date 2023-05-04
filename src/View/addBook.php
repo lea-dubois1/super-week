b1203e0b30de3d10300140f89cf21f50d2d2f2fd
lea-dubois1 <lea.dubois@laplateforme.io>
2023-05-04 11:00:16 +0200

    creation formulaire d'ajout de livre

---
+++ src/View/addBook.php
@@ -0,0 +1,5 @@
+<form action="books/write" method="POST">
+    <input type="text" name="titre" id="titre" placeholder="Titre" />
+    <textarea name="content" id="content" cols="30" rows="10" placeholder="Contenu..."></textarea>
+    <button type="submit">Publier</button>
+</form>
